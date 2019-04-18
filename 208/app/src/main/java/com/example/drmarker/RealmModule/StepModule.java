f5830ba149d343cef80157d5c1033d6acd6405ea
JTL <ljt1403345428@gmail.com>
2019-04-18 22:02:57 +0100

    Runnable all realm design re

---
+++ 208/app/src/main/java/com/example/drmarker/RealmModule/StepModule.java
@@ -0,0 +1,10 @@
+package com.example.drmarker.RealmModule;
+
+import com.example.drmarker.User;
+import com.example.drmarker.stepModel.StepModel;
+
+import io.realm.annotations.RealmModule;
+
+@RealmModule(classes = { StepModel.class })
+public class StepModule {
+}
