410d7d26617ab64ff4cd06a5c4c5d445cd1b3946
JTL <ljt1403345428@gmail.com>
2019-04-22 23:57:05 +0100

    user info module

---
+++ 208/app/src/main/java/com/example/drmarker/RealmModule/UserInfoModule.java
@@ -0,0 +1,10 @@
+package com.example.drmarker.RealmModule;
+
+import com.example.drmarker.userModel.UserInfomation;
+
+import io.realm.annotations.RealmModule;
+
+@RealmModule(classes = { UserInfomation.class })
+
+public class UserInfoModule {
+}
